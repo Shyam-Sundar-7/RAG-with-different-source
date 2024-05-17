cd495d07e415d9cc7c1560da21a8b318b73bbeea
shyamsundar009 <167984593+shyamsundar009@users.noreply.github.com>
2024-05-17 14:26:47 +0530

    second commit

---
+++ helper.py
@@ -0,0 +1,161 @@
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.document_loaders import UnstructuredHTMLLoader
+from langchain_community.document_loaders import Docx2txtLoader
+from langchain_community.document_loaders import UnstructuredPowerPointLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+import boto3
+import os
+from azure.storage.blob import BlobServiceClient
+from tqdm import tqdm
+from langchain.retrievers import ContextualCompressionRetriever
+from langchain.retrievers.document_compressors import CrossEncoderReranker
+from langchain_community.cross_encoders import HuggingFaceCrossEncoder
+from langchain_community.retrievers import BM25Retriever
+from langchain.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_openai import ChatOpenAI
+from operator import itemgetter
+from langchain.retrievers import EnsembleRetriever
+from langchain_openai import OpenAIEmbeddings
+from langchain_core.runnables import RunnableParallel
+import numpy as np
+from dotenv  import load_dotenv
+load_dotenv()
+import shutil
+
+def load_file(file_name):
+    loader=[]
+    print(file_name.split(".")[-1])
+    if file_name.split('.')[-1] == "pptx":
+        loader = UnstructuredPowerPointLoader(file_name).load()
+    elif file_name.split('.')[-1] == "pdf":
+        loader = PyPDFLoader(file_name).load()    
+    elif file_name.split('.')[-1] == "docx":
+        loader = Docx2txtLoader(file_name).load()
+    elif file_name.split('.')[-1] == "html":
+        loader = UnstructuredHTMLLoader(file_name).load()
+    text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=300,
+            chunk_overlap=0,
+            is_separator_regex=False,
+        )
+    pages = text_splitter.split_documents(loader)
+    return pages
+
+
+def file_to_chunks(folder):
+    pages=[]
+    for file_name in os.listdir(f"{folder}"):
+        pages.extend(load_file(f"{folder}\\{file_name}"))
+    if folder != "Local_data":
+        shutil.rmtree(f"{folder}")
+    return pages
+
+def azure_data_download(AZURE_CONNECTION_STRING,CONTAINER_NAME):
+    blob_service_client = BlobServiceClient.from_connection_string(AZURE_CONNECTION_STRING)
+    container_client = blob_service_client.get_container_client(CONTAINER_NAME)
+    if not os.path.exists("Azure_data"):
+        os.mkdir("Azure_data")
+    for file_name in container_client.list_blobs():
+        blob_client = container_client.get_blob_client(file_name)
+        with open(f"Azure_data\\{file_name.name}", "wb") as file:
+            data = blob_client.download_blob().readall()
+            file.write(data)
+
+
+def aws(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, BUCKET_NAME,object_name):
+        # Create an S3 client
+    s3 = boto3.client('s3',
+                    aws_access_key_id=AWS_ACCESS_KEY_ID,
+                    aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
+
+    # List objects in the bucket
+    response = s3.list_objects_v2(Bucket=BUCKET_NAME)
+
+    if not os.path.exists("S3_data"):
+        os.mkdir("S3_data")
+
+    # Download files in the 'data' object
+    for i in response.get('Contents',[]):
+        if i['Key'].split('/')[-1] != "" and i['Key'].split('/')[0] == object_name:
+            # print(i['Key'])
+            file_path = os.path.join("S3_data", i['Key'].split('/')[-1])
+            # print(file_path)
+            s3.download_file(BUCKET_NAME, i['Key'], file_path)
+
+
+def generate_queries():
+        
+    # Multi Query: Different Perspectives
+    template = """You are an AI language model assistant. Your task is to generate Four 
+    different versions of the given user question to retrieve relevant documents from a vector 
+    database. By generating multiple perspectives on the user question, your goal is to help
+    the user overcome some of the limitations of the distance-based similarity search. 
+    Provide these alternative questions separated by newlines. Original question: {question}"""
+    prompt_perspectives = ChatPromptTemplate.from_template(template)
+
+
+    generate_querie = (
+        prompt_perspectives 
+        | ChatOpenAI(temperature=0) 
+        | StrOutputParser() 
+        | (lambda x: x.split("\n"))
+    )
+    return generate_querie 
+
+
+def _get_docs(m):
+    docs=[]
+    docs.extend(m['original'])
+    for i in m['new']:
+        docs.extend(i)
+    return docs
+
+def keyword_extractor():
+    prompt="""
+    You are an AI language model assistant. Your task is to help the user retrieve keywords from their query. 
+
+    Please provide me with the keywords you would like to extract from your query. 
+
+    Keywords: {keywords}
+    """
+    prompt_perspectives=ChatPromptTemplate.from_template(prompt)
+    generate_querie = (
+        prompt_perspectives 
+        | ChatOpenAI(temperature=0) 
+        | StrOutputParser() )
+    return generate_querie
+
+def main(Query,chunks,db):
+    faiss_retriever=db.as_retriever(search_kwargs={'k': 10})
+
+    original_question= faiss_retriever
+    retrieval_chain =  generate_queries() | faiss_retriever.map()
+    map_chain = RunnableParallel(original=original_question,new=retrieval_chain) | _get_docs
+
+    Bm25_retriever = BM25Retriever.from_documents(chunks)
+    Bm25_retriever.k = 10
+
+    ensemble_retriever = EnsembleRetriever(
+    retrievers=[Bm25_retriever, map_chain], weights=[0.5, 0.5]
+    )
+
+    model = HuggingFaceCrossEncoder(model_name="cross-encoder/ms-marco-MiniLM-L-6-v2")
+    compressor = CrossEncoderReranker(model=model, top_n=4)
+    compression_retriever = ContextualCompressionRetriever(
+        base_compressor=compressor, base_retriever=ensemble_retriever
+    )
+
+    final_prompt="""You are an assistant for question-answering tasks. Use the following pieces of retrieved context to answer the question. If you don't know the answer, just say that you don't know. Use three sentences maximum and keep the answer concise.
+        Question: {question} 
+        Context: {context} 
+        Answer:"""
+
+    final_prompt_perspectives=ChatPromptTemplate.from_template(final_prompt)
+
+    llm_chain2=({"context": itemgetter("query") | compression_retriever,
+              "question":itemgetter("query")}
+             | final_prompt_perspectives
+             | ChatOpenAI(temperature=0) | StrOutputParser() )
+    
+    return llm_chain2.invoke({"query":Query})
--- main.py
+++ main.py
@@ -1,16 +1,92 @@
 import streamlit as st
-
+from helper import aws,file_to_chunks,azure_data_download,main
+from langchain_chroma import Chroma
+from langchain_openai import OpenAIEmbeddings
 st.title("RAG with different source")
-
+import time
 # Using object notation
 add_selectbox = st.sidebar.selectbox(
-    "How would you like to be contacted?",
-    ("Email", "Home phone", "Mobile phone")
+    "Which data source you are using?",
+    ("Azure Blob Storage", "Local storage","AWS S3 Bucket"),
+    index=None,
+    placeholder="Select contact method...",
 )
 
-# Using "with" notation
-with st.sidebar:
-    add_radio = st.radio(
-        "Choose a shipping method",
-        ("Standard (5-15 days)", "Express (2-5 days)")
-    )
+if add_selectbox == "Azure Blob Storage":
+    st.sidebar.write("You selected Azure Blob Storage.")
+    AZURE_CONNECTION_STRING = st.sidebar.text_input("Azure Connection String Input",type="password")
+    CONTAINER_NAME = st.sidebar.text_input("Azure Container Name")
+elif add_selectbox == "Local storage":
+    st.sidebar.write("You selected Local storage.")
+    LOCAL_PATH = st.sidebar.selectbox(
+    "Local Path",
+    ("Local_data","NIL"),
+    index=None,
+    placeholder="Select contact method...",
+)
+elif add_selectbox == "AWS S3 Bucket":
+    aws_access_key = st.sidebar.text_input("AWS Access Key",type="password")
+    aws_secret_access_key = st.sidebar.text_input("AWS SECRET ACCESS KEY",type="password")
+    bucket_name= st.sidebar.text_input("AWS BUCKET NAME")
+    object_name= st.sidebar.text_input("AWS OBJECT NAME")
+else:
+    st.sidebar.write("You selected nothing.")
+
+if st.sidebar.button("Injest"):
+    if add_selectbox == "Azure Blob Storage" and AZURE_CONNECTION_STRING and CONTAINER_NAME:
+        # Download PDF from Azure Blob Storage
+        with st.sidebar:
+            try:
+                with st.spinner("Azure connection is creating....."):
+                    azure_data_download(AZURE_CONNECTION_STRING=AZURE_CONNECTION_STRING, CONTAINER_NAME=CONTAINER_NAME)
+                st.session_state.pages = file_to_chunks("Azure_data")
+                with st.spinner("Chroma VectorDatabse is creating....."):
+                    st.session_state.db = Chroma.from_documents(st.session_state.pages, OpenAIEmbeddings(), persist_directory="Azure_Chroma_db")
+                st.success("Chroma VectorDatabse is created successfully in the Azure_Chroma_db")
+            except Exception as e:
+                st.error(f"Error connecting with Azure: {str(e)}")
+    elif add_selectbox == "Local storage" and LOCAL_PATH == "Local_data":
+        with st.sidebar:
+            with st.spinner("Local Folder documents to chunks are in the process.........."):
+                st.session_state.pages = file_to_chunks("Local_data")
+            with st.spinner("Chroma VectorDatabse is creating....."):
+                st.session_state.db = Chroma.from_documents(st.session_state.pages, OpenAIEmbeddings(), persist_directory="Local_Chroma_db")
+            st.success("Chroma VectorDatabse is created successfully in the Local_Chroma_db folder")
+    elif add_selectbox == "AWS S3 Bucket":
+        st.write("AWS S3 Bucket")
+        st.write(aws_access_key)
+        st.write(aws_secret_access_key)
+        st.write(bucket_name)
+        st.write(object_name)
+    st.session_state.injest = True
+
+
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+if "injest" not in st.session_state:
+    st.session_state.injest = False
+if "pages" not in st.session_state:
+    st.session_state.pages = []
+if "db" not in st.session_state:
+    st.session_state.db = []  
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# React to user input
+if prompt := st.chat_input("Lets have a chat with our document") and st.session_state.injest:
+    # Display user message in chat message container
+    st.chat_message("user").markdown(prompt)
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    answer=main(prompt,chunks=st.session_state.pages,db=st.session_state.db)
+    response = f"{answer}"
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        st.markdown(response)
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": response})
