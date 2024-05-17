18a5693a6bcd591b46872556551738fb5926a6de
shyamsundar009 <167984593+shyamsundar009@users.noreply.github.com>
2024-05-17 13:01:24 +0530

    fitst commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.title("RAG with different source")
+
+# Using object notation
+add_selectbox = st.sidebar.selectbox(
+    "How would you like to be contacted?",
+    ("Email", "Home phone", "Mobile phone")
+)
+
+# Using "with" notation
+with st.sidebar:
+    add_radio = st.radio(
+        "Choose a shipping method",
+        ("Standard (5-15 days)", "Express (2-5 days)")
+    )
