153925a453176ecfa19e44520a9580a1a10ddc87
shyamsundar009 <167984593+shyamsundar009@users.noreply.github.com>
2024-05-22 16:58:19 +0530

    still json to history need to be added

--- main.py
+++ main.py
@@ -49,18 +49,18 @@ if st.sidebar.button("Injest"):
                     azure_data_download(AZURE_CONNECTION_STRING=AZURE_CONNECTION_STRING, CONTAINER_NAME=CONTAINER_NAME)
                 with st.spinner("Azure Folder documents to chunks are in the process.........."):
                     st.session_state.pages = file_to_chunks("Azure_data")
-                with st.spinner("Chroma VectorDatabse is creating....."):
+                with st.spinner("VectorDatabse is creating....."):
                     st.session_state.db = Chroma.from_documents(st.session_state.pages, OpenAIEmbeddings(), persist_directory="Azure_Chroma_db")
-                st.success("Chroma VectorDatabse is created successfully in the Azure_Chroma_db")
+                st.success("VectorDatabse is created successfully in the Azure_Chroma_db")
             except Exception as e:
                 st.error(f"Error connecting with Azure: {str(e)}")
     elif add_selectbox == "Local storage" and LOCAL_PATH == "Local_data":
         with st.sidebar:
             with st.spinner("Local Folder documents to chunks are in the process.........."):
                 st.session_state.pages = file_to_chunks("Local_data")
-            with st.spinner("Chroma VectorDatabse is creating....."):
+            with st.spinner("VectorDatabse is creating....."):
                 st.session_state.db = Chroma.from_documents(st.session_state.pages, OpenAIEmbeddings(), persist_directory="Local_Chroma_db")
-            st.success("Chroma VectorDatabse is created successfully in the Local_Chroma_db folder")
+            st.success("VectorDatabse is created successfully in the Local_Chroma_db folder")
     elif add_selectbox == "AWS S3 Bucket":
         with st.sidebar:
             try:
@@ -68,9 +68,9 @@ if st.sidebar.button("Injest"):
                     aws(AWS_ACCESS_KEY_ID=aws_access_key, AWS_SECRET_ACCESS_KEY=aws_secret_access_key, BUCKET_NAME=bucket_name, object_name=object_name)
                 with st.spinner("AWS Folder documents to chunks are in the process.........."):
                     st.session_state.pages = file_to_chunks()
-                with st.spinner("Chroma VectorDatabse is creating....."):
+                with st.spinner("VectorDatabse is creating....."):
                     st.session_state.db = Chroma.from_documents(st.session_state.pages, OpenAIEmbeddings(), persist_directory="AWS_Chroma_db")
-                st.success("Chroma VectorDatabse is created successfully in the AWS_Chroma_db folder")
+                st.success("VectorDatabse is created successfully in the AWS_Chroma_db folder")
             except Exception as e:
                 st.error(f"Error in connecting with AWS: {str(e)}")
     else:
@@ -78,7 +78,6 @@ if st.sidebar.button("Injest"):
 
     st.session_state.injest = True
     # st.session_state
-
 
 
 # Initialize chat history
@@ -107,11 +106,13 @@ if prompt and st.session_state.injest:
     questions=generate_queries(prompt).invoke(prompt)
     keywords=keyword_extractor().invoke(prompt)
     with st.spinner(f"""Creative Query from the original question \n{'\n'.join(questions[1:])} \n\n Keywords from the original question \n\n{keywords}\n"""):
-        questions.append(prompt)
-
         answer_dict=main(prompt,chunks=st.session_state.pages,db=st.session_state.db)
         answer=answer_dict["response"]
-        metadata=meta(answer_dict["context"])
+        metadata=[]
+        if answer =="I don't know.":
+            metadata=["No relevant Documents correspond to the question. Please try with different query."]
+        else:
+            metadata=meta(answer_dict["context"])
     response = f"{answer} \n\n\n Metadata: \n\n{'\n\n'.join(metadata)}"
     # Display assistant response in chat message container
     with st.chat_message("assistant"):
